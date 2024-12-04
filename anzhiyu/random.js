464ed7891de776d7bf24c7c64bfc4aedc2a92079
Andy Li <nerdylinaive@gmail.com>
2024-12-03 21:50:28 -0600

    Site updated: 2024-12-03 21:50:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/03/hello-world/","2024/12/03/About-me/"];function toRandomPost(){
+var posts=["2024/12/03/About-me/","2024/12/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
