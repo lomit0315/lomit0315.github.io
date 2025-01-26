02d22dc506e08f65cffca0455e1b030a15f34a3c
Andy Li <nerdylinaive@gmail.com>
2025-01-26 10:43:12 -0600

    Site updated: 2025-01-26 10:43:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/26/ABC367/","2025/01/26/AGC020C-Median-Sum/","2024/12/03/About-me/","2024/12/03/hello-world/"];function toRandomPost(){
+var posts=["2025/01/26/ABC367/","2025/01/26/AGC020C-Median-Sum/","2024/12/03/hello-world/","2024/12/03/About-me/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
